4c485a402da70b01f64a5fc3f9cd537bdbe9cd26
kk412027247 <kk412027247@gmail.com>
2018-10-15 21:17:12 +0800

    5-4

--- retriever/main.go
+++ retriever/main.go
@@ -11,14 +11,47 @@ type Retriever interface{
 	Get(url string) string
 }
 
+type Poster interface {
+	// 最后面的string表示返回值是string
+	Post(url string, from map[string]string) string
+}
+
+const url =  "http://www.imooc.com"
 
 func download (r Retriever) string {
-	return r.Get("http://www.imooc.com")
+	return r.Get(url)
+}
+
+
+func post (poster Poster) {
+	poster.Post(url,map[string]string{
+		"name":"ccmouse",
+		"course":"golang",
+	})
+}
+
+
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
+
+func session (s RetrieverPoster) string {
+
+	s.Post(url,map[string]string{
+		"Contents":"another fake imooc.com",
+	})
+	return s.Get(url)
 }
 
 func main() {
 	var r Retriever
-	r = mock.Retriever{"this is a fake imooc.com"}
+	// 这里的 mock retriever 这个struct 同时实现了  Retriever 与   Poster 接口的方法，所以根据duck typing 原理
+	// 这个  retriever 就是 同时继承了 Retriever 与 Poster
+	// r 只是 Retriever的继承，无法同时调用 get 和 post方法，但是可以
+	retriever := mock.Retriever{"this is a fake imooc.com"}
+	r = &retriever
 	inspect(r)
 	r = &real.Retriever{
 		UserAgent:"Mozilla/5.0",
@@ -38,12 +71,15 @@ func main() {
 
 
 	//fmt.Println(download(r))
+	fmt.Println("try a session")
+
+	fmt.Println(session(&retriever))
 }
 
 func inspect(r Retriever) {
 	fmt.Printf("%T %v\n", r, r)
 	switch v := r.(type) {
-	case mock.Retriever:
+	case *mock.Retriever:
 		fmt.Println("Contents: ", v.Contents)
 	case *real.Retriever:
 		fmt.Println("UserAgent: ", v.UserAgent)
--- retriever/mock/mockretriever.go
+++ retriever/mock/mockretriever.go
@@ -4,7 +4,14 @@ type Retriever struct {
  	Contents string
 }
 
-func (r Retriever) Get(url string) string {
+
+// duck typing 的写法，只要是用了接口的方法，默认就是接口的继承
+func (r *Retriever) Post(url string, from map[string]string) string {
+	r.Contents = from["Contents"]
+	return "ok"
+}
+
+func (r *Retriever) Get(url string) string {
 	return r.Contents	
 }
 
